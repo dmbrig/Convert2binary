e6340bf9af927c8b69c3a2cf9e1316a9b10d3a58
david <dmbrigida@gmail.com>
2022-04-15 12:41:06 -0400

    Working division

---
+++ uh.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main() 
+{
+	
+	int a;
+
+	printf("Enter input\n");
+	scanf("%d", &a);
+	// quotient is
+	int c = a / 2;
+	// remainder is
+	int d = a % 2;
+	printf("The quotient is %d\n", c);
+	printf("The remainder is %d\n", d);
+
+
+
+	
+
+}
