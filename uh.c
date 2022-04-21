1209e9e00b85b7116324c381718c726d1c32b087
david <dmbrigida@gmail.com>
2022-04-20 21:07:48 -0400

    Printing reversed binary.

--- uh.c
+++ uh.c
@@ -7,14 +7,31 @@ int main()
 	printf("Enter input\n");
 	scanf("%d", &a);
 	// quotient is
-	int c = a / 2;
+	//int c = a / 2;
 	// remainder is
-	int d = a % 2;
-	printf("The quotient is %d\n", c);
-	printf("The remainder is %d\n", d);
+	//int d = a % 2;
+	//printf("The quotient is %d\n", c);
+	//printf("The remainder is %d\n", d);
+	int b = a;
 
+	// daddy stuff \/
 
-
+	int i = 0;
+	while (a > 0) {
+		a = a / 2;
+		i++;
+	}
+	//printf("The length of the number is %d\n", i);
 	
-
+	int j = 0;
+	int binary[i];
+	int d;
+	while (b > 0) {
+		d = b % 2;
+		printf("%d", d);
+		binary[j] = d;
+		b = b / 2;
+		j++;
+	}
+	return 0;
 }
